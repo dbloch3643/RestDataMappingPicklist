81e5786301866ea7f356f52b7f8132cd756c1087
Doug Bloch <doug@bloch.com>
2022-11-15 12:32:08 -0500

    Made changes to fit our needs

--- src/RestServiceData.ts
+++ src/RestServiceData.ts
@@ -1,7 +1,6 @@
 import * as SDK from 'azure-devops-extension-sdk';
-import { WorkItemTrackingServiceIds, IWorkItemFormService } from "azure-devops-extension-api/WorkItemTracking/WorkItemTrackingServices";
+import { WorkItemTrackingServiceIds, IWorkItemFormService } from 'azure-devops-extension-api/WorkItemTracking/WorkItemTrackingServices';
 import { LoadDataFromService } from './rest-call';
-import get from 'lodash/get';
 import { AxiosResponse } from 'axios';
 
 export class RestServiceData {
@@ -9,26 +8,27 @@ export class RestServiceData {
   public data = [];
 
   public async getSuggestedValues(): Promise<string[]> {
-    var resp: AxiosResponse<any>;
+    const jp = require('jsonpath');
+    let resp: AxiosResponse<any>;
     try {
       resp = await LoadDataFromService();
     } catch (error) {
       return Promise.resolve([]);
     }
-    const keyFieldName = SDK.getConfiguration().witInputs.RestServiceKeyField;
-    const arrayPath = SDK.getConfiguration().witInputs.PathToArray;
+    const jsonquery = SDK.getConfiguration().witInputs.JsonPathQuery;
     if (resp !== undefined && resp.data !== undefined) {
-      var arrayData = get(resp.data, arrayPath, resp.data);
+
+      var arrayData = jp.query(resp.data, jsonquery);
+
     }
     if (arrayData) {
       if (arrayData.constructor !== Array) {
         console.dir(arrayData);
-        console.error("response is not an array ^");
+        console.error('response is not an array ^');
         return Promise.resolve([]);
       }
       this.data = arrayData;
-      var fieldSet = arrayData.map((a: any) => a[keyFieldName]);
-      return [...new Set<string>(fieldSet.sort())];
+      return [...new Set<string>(arrayData)];
     }
     else {
       // if the values input were not specified as an input, get the suggested values for the field.
@@ -36,4 +36,5 @@ export class RestServiceData {
       return await service.getAllowedFieldValues(SDK.getConfiguration().witInputs.FieldName) as string[];
     }
   }
+
 }
